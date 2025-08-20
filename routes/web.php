c82c9bd5d77efb313649cfe5b7393d81aa504acf
maxheong54 <maxheong54@gmail.com>
2025-08-20 22:31:02 +0300

    lesson 2

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "Hello world";
+});
+
+Route::get('about', function (): string {
+    return view('about');
 });
