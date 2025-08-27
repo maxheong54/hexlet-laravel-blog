a27d86f339757b30e2910d1a2d0bc52c5f6967ed
maxheong54 <maxheong54@gmail.com>
2025-08-27 20:15:00 +0300

    lesson 11

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/', function () {
 });
 
 Route::get('about', [PageController::class, 'about'])->name('about');
+Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
